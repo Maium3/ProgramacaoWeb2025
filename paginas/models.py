fd6f92dd0d3a5a98bd02398fb7b2a8c9ded7d3d4
GuilhermeAlmeidaRufino <20223020039@estudantes.ifpr.edu.br>
2025-05-14 15:57:35 -0300

    comecei esta merda de models

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(100)
+    data_nasc = models.DateField
+    email = models.EmailField
+    nickname = models.CharField(100)
+    id = models.CharField(16)
+    conversa = models.ForeignKey(Conve)
+    
+    
+
+class Universo(models.Model):
+    nome = models.CharField(255)
+    descricao = models.TextField(1500)
+    
+class Personagem(models.Model):
+    nome = models.TextField(100)
+    habilidades = models.TextField(1000)
+    caracteristicas = models.TextField(1000)
+    origem = models.TextField(1500)
+    user = models.ForeignKey(PerfilUsuario, on_delete= Protect)
+    
+class Conversa(models.Model):
+    usuarios = models.ForeignKey(Usuario, on_delete=Protect)
+    u 
